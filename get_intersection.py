07ec92cb65f0b1d284dff07543ef9648431f7865
onion-rain <1934455602@qq.com>
2020-08-28 09:28:45 +0800

    get instersection新增命令行接口

--- get_intersection.py
+++ get_intersection.py
@@ -6,6 +6,11 @@ from xlrd import *
 import win32com.client
 import csv
 import sys
+import traceback
+
+# D:\Python\excel\Scripts\Activate.ps1
+# pyinstaller --onefile --nowindowed get_intersection.py
+
 
 def get_table(excel_path, sheet=0):
     data = xlrd.open_workbook(excel_path)
@@ -19,14 +24,51 @@ def write_row(ws, sh, index, cursor):
     cursor = cursor + 1
     return cursor
 
-if __name__ == '__main__':
-    in1_path = 'io/2018年档案入库情况20200812.xls' # in
-    in2_path = 'io/2020年新增就业登记情况（截止0803）_nopassword.xlsx' # in
-    in1 = get_table(in1_path)
-    in2 = get_table(in2_path)
+def check(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-    in1_idcard_value = in1.col_values(2)[1:]
-    in2_idcard_value = in2.col_values(1)[1:]
+if __name__ == '__main__':
+    print("default:")
+    print("   excel1 path: io/in1.xlsx")
+    print("   excel2 path: io/in2.xlsx")
+    print("   save root: io")
+    print("   excel1 ID NO col: 0")
+    print("   excel2 ID NO col: 0")
+    in1_path = input("excel1 path:")
+    if in1_path == "":
+        in1_path = 'io/in1.xlsx' # in1
+    in2_path = input("excel2 path:")
+    if in2_path == "":
+        in2_path = 'io/in2.xlsx' # in2
+    in1_path = in1_path.replace("\\", "/")
+    in2_path = in2_path.replace("\\", "/")
+    try:
+        in1 = get_table(in1_path)
+        in2 = get_table(in2_path)
+    except FileNotFoundError:
+        input(traceback.print_exc())
+        exit(0)
+
+    save_root = input("save root:")
+    if save_root == "":
+        save_root = "io"
+    save_root = save_root.replace("\\", "/") + "/"
+    check(save_root)
+
+    in1_col = input("excel1 ID NO col:")
+    if in1_col == "":
+        in1_col = '0' # in1 ID NO col
+    in2_col = input("excel2 ID NO col:")
+    if in2_col == "":
+        in2_col = '0' # in2 ID NO col
+    in1_col = int(in1_col)
+    in2_col = int(in2_col)
+
+    in1_idcard_value = in1.col_values(in1_col)[1:]
+    in2_idcard_value = in2.col_values(in2_col)[1:]
+
+    print("\nfound: 0", end="")
 
     # 仅凭id card求得交集
     union_id = [i1 for i1 in in1_idcard_value if i1 in in2_idcard_value]
@@ -39,67 +81,15 @@ if __name__ == '__main__':
     new_in2_sheet0 = new_in2.add_sheet('sheet0')
     new_in2_cursor = write_row(new_in2_sheet0, in2, 0, 0)
     new_in2_cursor = 1
-    # new_out = xlwt.Workbook()
-    # new_out_sheet = new_out.add_sheet('sheet0')
-    # new_out_cursor = 0
+    num = 1
     for id in union_id:
+        print("\rfound: {}".format(num), end="")
         new_in1_cursor = write_row(new_in1_sheet0, in1, in1_idcard_value.index(id)+1, new_in1_cursor)
         new_in2_cursor = write_row(new_in2_sheet0, in2, in2_idcard_value.index(id)+1, new_in2_cursor)
-        # new_out_cursor = write_row(new_out_sheet, out, out_idcard_value.index(id)+1, new_out_cursor)
+        num += 1
 
-    # new_out.save('new_out.xls')
-    print("id done")
+    new_in1.save(save_root + 'new_in1.xls')
+    new_in2.save(save_root + 'new_in2.xls')
 
-    
-
-
-    # # 第一轮比较完后剩余需要比对的行号
-    # in1_rest_rawx = [i for i, x in enumerate(in1_idcard_value) if x=='']
-    # in2_rest_rawx = [i for i, x in enumerate(in2_idcard_value) if x not in union_id]
-
-    # # 排除out与in1、in2 重合id
-    # in1_id_in_out = [x for x in in1_idcard_value if x in out_idcard_value]
-    # in2_id_in_out = [x for x in in2_idcard_value if x in out_idcard_value]
-    # in1_id_in_out_rawx = []
-    # in2_id_in_out_rawx = []
-    # for id in in1_id_in_out:
-    #     in1_id_in_out_rawx.append(in1_idcard_value.index(id))
-    # for id in in2_id_in_out:
-    #     in2_id_in_out_rawx.append(in2_idcard_value.index(id))
-    # in1_rest_rawx = [x for x in in1_rest_rawx if x not in in1_id_in_out_rawx]
-    # in2_rest_rawx = [x for x in in2_rest_rawx if x not in in2_id_in_out_rawx]
-
-    # # 求name交集
-    # in1_name_value = in1.col_values(0)[1:]
-    # in2_name_value = in2.col_values(1)[1:]
-    # in1_name_rest_value = [x for i, x in enumerate(in1_name_value) if i in in1_rest_rawx]
-    # in2_name_rest_value = [x for i, x in enumerate(in2_name_value) if i in in2_rest_rawx]
-    # union_name = [i1 for i1 in in1_name_rest_value if i1 in in2_name_rest_value]
-
-    # # name交集所在行号
-    # union_in1_name_rawx = []
-    # union_in2_name_rawx = []
-    # for name in union_name:
-    #     in1_name_rawx = [i for i, x in enumerate(in1_name_value) if x==name]
-    #     in2_name_rawx = [i for i, x in enumerate(in2_name_value) if x==name]
-    #     union_1_raw_x = [x for x in in1_name_rawx if x in in1_rest_rawx]
-    #     union_2_raw_x = [x for x in in2_name_rawx if x in in2_rest_rawx]
-
-    #     if len(union_1_raw_x) > 0 and len(union_2_raw_x) > 0:
-    #         union_in1_name_rawx += union_1_raw_x
-    #         union_in2_name_rawx += union_2_raw_x
-
-    
-    # new_in1_sheet1 = new_in1.add_sheet('sheet1')
-    # new_in1_cursor = write_row(new_in1_sheet1, in1, 0, 0)
-    # new_in1_cursor = 1
-    # new_in2_sheet1 = new_in2.add_sheet('sheet1')
-    # new_in2_cursor = write_row(new_in2_sheet1, in2, 0, 0)
-    # new_in2_cursor = 1
-    # for rawx in union_in1_name_rawx:
-    #     new_in1_cursor = write_row(new_in1_sheet1, in1, rawx+1, new_in1_cursor)
-    # for rawx in union_in2_name_rawx:
-    #     new_in2_cursor = write_row(new_in2_sheet1, in2, rawx+1, new_in2_cursor)
-
-    new_in1.save('io/_new_in1.xls')
-    new_in2.save('io/_new_in2.xls')
+    print("\n\ndone")
+    input("任意键结束...")
